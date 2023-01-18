539052f07fd93d760d262683ee8c1a458e01ab24
MendesMaquiansPCP <rafael.gois@mendesmaquinas.com.br>
2023-01-18 17:41:59 -0300

    update funcionamento

--- app/src/main/java/com/example/carregandoimg/Tarefa.java
+++ app/src/main/java/com/example/carregandoimg/Tarefa.java
@@ -1,26 +1,67 @@
 package com.example.carregandoimg;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 
-public class Tarefa extends AsyncTask<Void, Void, Void> {
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Tarefa extends AsyncTask<String, String, Bitmap> {
+
+    private ProgressDialog dialog;
+    private Context context;
+    private TarefaInterface ti;
+
+    public Tarefa(Context context, TarefaInterface ti) {
+        this.context = context;
+        this.ti = ti;
+    }
 
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
+
+        dialog = new ProgressDialog(context);
+        dialog.setMessage("Baixando imagem");
+        dialog.show();
+
+    }
+
+
+    @Override
+    protected Bitmap doInBackground(String... params) {
+        //todo testar se colocar isso dentro do for, cada execucao de atalizacao da of chama o doinbackground
+
+        Bitmap img = null;
+
+        try {
+            URL url = new URL(params[0]);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            InputStream input = connection.getInputStream();
+            img = BitmapFactory.decodeStream(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return(img);
     }
 
     @Override
-    protected Void doInBackground(Void... voids) {
-        return null;
+    protected void onProgressUpdate(String... params) {
+        super.onProgressUpdate(params);
     }
 
     @Override
-    protected void onProgressUpdate(Void... values) {
-        super.onProgressUpdate(values);
-    }
+    protected void onPostExecute(Bitmap bitmap) {
+        super.onPostExecute(bitmap);
 
-    @Override
-    protected void onPostExecute(Void unused) {
-        super.onPostExecute(unused);
+        dialog.setMessage("Imagem carregada!");
+        ti.depoisDownload(bitmap);
+        dialog.dismiss();
     }
 }
